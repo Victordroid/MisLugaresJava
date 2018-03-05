90ef16a7616d74ba6346b19cc0111e3cef353b9c
Victordroid <36991460+Victordroid@users.noreply.github.com>
2018-03-05 10:39:31 +0100

    Initial commit

---
+++ Mislugares/src/main/java/coordenadas_geograficas/com/example/mislugares/TipoLugar.java
@@ -0,0 +1,29 @@
+package coordenadas_geograficas.com.example.mislugares;
+
+/**
+ * Created by Victor on 08/02/2018.
+ */
+
+public enum TipoLugar {
+    OTROS("Otros", 5),
+    RESTAURANTE("Restaurante", 2),
+    BAR("Bar", 6),
+    COPAS("Copas", 0),
+    ESPECTACULO("Espectáculo", 0),
+    HOTEL("Hotel", 0),
+    COMPRAS("Compras", 0),
+    EDUCACION("Educación", 0),
+    DEPORTE("Deporte", 0),
+    NATURALEZA("Naturaleza", 0),
+    GASOLINERA("Gasolinera", 0);
+
+    private final String texto;
+    private final int recurso;
+
+    TipoLugar(String texto, int recurso) {
+        this.texto = texto;
+        this.recurso = recurso;
+    }
+    public String getTexto() { return texto; }
+    public int getRecurso() { return recurso; }
+}
